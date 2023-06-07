3456551eccb45bbda1af7e3afd32acf165ce1cb1
main <>
2023-06-07 11:42:26 +0900

    interface

---
+++ Unity3D/Assets/WayPoint/Node.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour, parent
+{
+    public void zzzzzzzzzzz(string str)
+    {
+        Debug.Log(str);
+    }
+
+    private bool Check;
+
+    void Start()
+    {
+        zzzzzzzzzzz("Ω√¿€!!");
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Unity3D/Assets/WayPoint/parent.cs
@@ -0,0 +1,5 @@
+
+public interface parent 
+{
+    public void zzzzzzzzzzz(string str);
+}
